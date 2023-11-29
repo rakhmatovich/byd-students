419018100cf58d791f03817ff82b7a368c674fbd
rakhmatovich <nurmuxammadrahmatov@gmail.com>
2023-11-29 17:17:00 +0500

    added tailwind

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Layout from '../components/Layout'
 export default function Home() {
   return (
     <Layout>
-        <h1>Byd Home</h1>
+        <h1 className='text-3xl text-center text-cyan-400 font-bold'>Byd Home</h1>
     </Layout>
   )
 }
